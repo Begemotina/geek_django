3aac8ce56642ece55e567ba87fc1ebe8287d9696
Begemotina <Begemotina@nightmail.ru>
2020-08-24 05:20:55 +0300

    цикл тоже готов

--- lesson_2/begeshop/mainapp/views.py
+++ lesson_2/begeshop/mainapp/views.py
@@ -11,14 +11,37 @@ def index(request):
 
 
 def cotalogs(request):
+    light_items = [
+        {
+            'href1': "#",
+            'image': "{% static 'img/csm_esprite-fs-01_1a739cd840.png' %}",  # а что сдесь надо что бы заработало?
+            'title': "esprite",
+            'href2': "#",
+            'name': "ESPRITE™ FS",
+        },
+    ]
     context = {
         'title_page': 'каталог хатки',
+        'light_items': light_items,
     }
     return render(request, 'mainapp/cotalogs.html', context)
 
 
 def contacts(request):
+    places = [
+        {
+            'phone': 'Контактный номер телефона интернет-магазина: 17-85-00',
+            'address': 'Aдрес: туда-туда, потом на лево, через поле, направо от третьей пальмы хатка 15',
+            'email': 'Begemotina@nightmail.ru',
+        },
+        {
+            'phone': 'Контактный номер телефона интернет-палатки: 1111-00-222',
+            'address': 'Aдрес: Красная площадь, дом 1',
+            'email': 'Begemota_begemot@nightmail.ru',
+        },
+    ]
     context = {
         'title_page': 'контаткты хатки',
+        'places': places,
     }
     return render(request, 'mainapp/contacts.html', context)
